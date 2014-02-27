685c8cc1f87a4227d716674efca4ecb88beddcb2
Konstantin Nosov <knosov@inceptumsoft.com>
2014-02-27 17:28:01 +0400

    initial commit

---
+++ src/SolutionInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyCompany("InceptumSoft")]
+[assembly: AssemblyProduct("Inceptum.AppServer.RavenDb")]
+[assembly: AssemblyCopyright("Copyright © InceptumSoft 2013")]
+[assembly: AssemblyTrademark("Inceptum.AppServer.RavenDb")]
+[assembly: AssemblyCulture("")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: InternalsVisibleTo("Inceptum.AppServer.Tests")]
